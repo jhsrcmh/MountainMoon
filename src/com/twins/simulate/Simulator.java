c308c708d46fd3fce62ef6fb35d59dd34874130d
jhsrcmh <jhsrcmh1992@gmail.com>
2012-12-13 11:09:32 +0800

    '11'

--- src/com/twins/simulate/Simulator.java
+++ src/com/twins/simulate/Simulator.java
@@ -2,8 +2,6 @@ package com.twins.simulate;
 
 import java.io.BufferedWriter;
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
